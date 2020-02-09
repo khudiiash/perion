4b3f3a76ddb8342e614fa55493891d6b1557b7c1
Dmytro <dmytro.khudiyash@gmail.com>
2020-02-09 08:47:19 +0200

    init

--- src/components/OneGameScreen/OneGameScreen.jsx
+++ src/components/OneGameScreen/OneGameScreen.jsx
@@ -19,43 +19,50 @@ const OneGameScreen = ({start,zIndex}) => {
         to: {left: start ? '0' : '-100%'},
         from: {left: start ? '-100%' : '0'},
         delay: start ? 2000 : 0,
-        config: { duration: 2000, easing: easings.easeQuad}})
+        config: { duration: 2000, easing: easings.easeQuad}
+    })
 
     // Right Crowd Animation
     let moveRightCrowd = useSpring({
         to: {right: start ? '0' : '-100%'},
         from: {right: start ? '-100%' : '0'},
         delay: start ? 2000 : 0,
-        config: { duration: 2000, easing: easings.easeQuad}})
+        config: { duration: 2000, easing: easings.easeQuad}
+    })
     
         // Text Animation
     let appearOne = useSpring({
         to: {opacity: start ? 1 : 0},
         from: {opacity: start ? 0 : 1},
         delay: start ? 4000 : 0,
-        config: {duration: start ? 1000 : 700}})
+        config: {duration: start ? 1000 : 700}
+    })
     let moveGame = useSpring({
         to: {opacity: start ? 1 : 0,transform: start ? 'translateY(0)' : 'translateY(55%)'},
         from: {opacity: start ? 0 : 1,transform: start ? 'translateY(55%)' : 'translateY(0)'},
         delay: start ? 4000 : 0,
-        config: { duration: start ? 1000: 500, easing: easings.easeQuadIn}})
+        config: { duration: start ? 1000: 500, easing: easings.easeQuadIn}
+    })
     let moveGlory = useSpring({
         to: {opacity: start ? 1 : 0,transform: start ? 'translateY(0)' : 'translateY(100%)'},
         from: {opacity: start ? 0 : 1,transform: start ? 'translateY(100%)' : 'translateY(0)'},
         delay: start ? 4500 : 0,
-        config: { duration: start ? 1200: 300, easing: easings.easeQuad}})
+        config: { duration: start ? 1200: 300, easing: easings.easeQuad}
+    })
     
         // Logo Animation
     let moveTimbers = useSpring({
         to: {opacity: start ? 1 : 0,transform: start ? 'translateX(0) scale(1)':'translateX(-50%) scale(1)'},
         from: {opacity: start ? 0 : 1,transform: start ? 'translateX(-50%) scale(4)':  'translateX(0) scale(1)'},
         delay: start ? 5000 : 0,
-        config: { duration: 1000, easing: easings.easeQuad}})
+        config: { duration: 1000, easing: easings.easeQuad}
+    })
     let moveDallas = useSpring({
         to: {opacity: start ? 1 : 0,transform: start ? 'translateX(0) scale(1)':'translateX(50%) scale(1)'},
         from: {opacity: start ? 0 : 1,transform: start ? 'translateX(50%) scale(4)':  'translateX(0) scale(1)'},
         delay: start ? 5000 : 0,
-        config: { duration: 1000, easing: easings.easeQuad}})
+        config: { duration: 1000, easing: easings.easeQuad}
+    })
 
     return (
         <div className='one-game-screen' style={{zIndex}}>
@@ -72,7 +79,7 @@ const OneGameScreen = ({start,zIndex}) => {
             </div>
            
             <animated.div style={moveTimbers} className="one-game-screen__timbers">
-                <img className ="one-game-screen__dallas-logo" src={TimbersLogo}/>
+                <img className ="one-game-screen__timbers-logo" width='100%' height='100%' src={TimbersLogo}/>
             </animated.div>
             <animated.div style={moveDallas} className="one-game-screen__dallas">
                 <img className ="one-game-screen__dallas-logo" src={DallasLogo}/>
